08a2bb216bee0bf7b322dc627ef54dcf265b954c
Charlie Vieth <cviethjr@pivotal.io>
2016-11-03 09:58:20 -0400

    wip
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

---
+++ scripts/build-winsw-pipe.ps1
@@ -0,0 +1,28 @@
+trap {
+  write-error $_
+  exit 1
+}
+
+$env:GOPATH = Join-Path -Path $PWD "gopath"
+$env:PATH = $env:GOPATH + "/bin;C:/go/bin;" + $env:PATH
+
+
+if ((Get-Command "go.exe" -ErrorAction SilentlyContinue) -eq $null)
+{
+  Write-Host "Installing Go 1.7.3!"
+  Invoke-WebRequest https://storage.googleapis.com/golang/go1.7.3.windows-amd64.msi -OutFile go.msi
+
+  $p = Start-Process -FilePath "msiexec" -ArgumentList "/passive /norestart /i go.msi" -Wait -PassThru
+
+  if($p.ExitCode -ne 0)
+  {
+    throw "Golang MSI installation process returned error code: $($p.ExitCode)"
+  }
+  Write-Host "Go is installed!"
+}
+
+
+cd $env:GOPATH/src/github.com/pivotal-cf-experimental/winsw-pipe
+ginkgo.exe
+cd -
+go.exe build -o winsw-pipe-output/winsw-pipe-$version.exe $env:GOPATH/src/github.com/pivotal-cf-experimental/winsw-pipe/main.go
--- scripts/collect-and-zip-agent-deps.sh
+++ scripts/collect-and-zip-agent-deps.sh
@@ -7,6 +7,7 @@ DEPS_VERSION=$(cat version/number | tr -d '[[:space:]]')
 mv tar/tar{-*,}.exe
 mv zlib1/zlib1{-*,}.dll
 mv winsw-exe/{winsw-*,job-service-wrapper}.exe
+mv winsw-pipe-exe/{winsw-pipe-*,pipe}.exe
 mv bosh-blobstore-dav/{davcli-*-windows-amd64,bosh-blobstore-dav}.exe
 mv bosh-blobstore-s3/{s3cli-*-windows-amd64,bosh-blobstore-s3}.exe
 
@@ -14,5 +15,6 @@ zip -j "bosh-agent-deps-zip/agent-dependencies-v$DEPS_VERSION.zip" \
 	bosh-blobstore-dav/*.exe \
 	bosh-blobstore-s3/*.exe \
 	winsw-exe/*.exe \
+	winsw-pipe-exe/*.exe \
 	tar/*.exe \
 	zlib1/*.dll
